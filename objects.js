d65bb606a65cf376231a82fdb2706dcf18f7ca74
Justin <jkimm516@gmail.com>
2018-04-21 07:30:01 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,6 +4,6 @@ function updatePlaylist(playlistName, artistName, songName) {
   return playlistName[artistName] = songName
 }
 
-function removeFromPlaylist(playlistName, artistName, songName) {
-  return playlistName.delete(ar)
+function removeFromPlaylist(playlistName, artistName) {
+  return playlistName.delete(artistName)
 }
